0ce7839176bf8a26335fd2aac1ba2a41e3f4a6ec
Danil <danil_ignatyev@mail.ru>
2021-02-13 11:14:41 +0700

    Change format from .c to .cpp

---
+++ chessviz.cpp
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+usnig namespace std;
+
+const int N = 10;
+void CheckDesk( int a[])
+{
+	int i, j;
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; i <N; i++)
+			cout << a[i][j];
+	}
+}
+void FirstPose(int a[])
+{
+	
+}
+
+
