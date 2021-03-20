fac76d12d8b1a17bf053b6ff75cd0d9eeefc52e9
Danil <danil_ignatyev@mail.ru>
2021-03-20 10:38:15 +0700

    Add code in desk.c

---
+++ src/libchessviz/desk.c
@@ -0,0 +1,15 @@
+#include <libchessviz/chessviz.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+const int true = 1, false = 0;
+void Print_table(char Table[9][9]) // Отрисовка доски
+{
+    int i, j;
+    for (i = 0; i < 9; i++) {
+        for (j = 0; j < 9; j++) {
+            printf("%c", Table[i][j]);
+        }
+        printf("\n");
+    }
