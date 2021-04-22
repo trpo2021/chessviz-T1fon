2aa8ae0832b8be6bfeeb715f5873cca895b2dbfe
Danil <danil_ignatyev@mail.ru>
2021-04-22 14:38:11 +0700

    Add code

--- src/chessviz/main.c
+++ src/chessviz/main.c
@@ -1,27 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <libchessviz/tools.h>
 #include <libchessviz/desk.h>
 #include <libchessviz/move.h>
 #include <libchessviz/picture.h>
+#include <libchessviz/tools.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main ()
+int main()
 {
+    unsigned int p = 0;
+    char Table_chess[9][9] = {};
+    //
+    Preparation(Table_chess); // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
+    //
+    FILE* file = 0;
+    char Name_file[30];
 
-	unsigned int p = 0;
-	char Table_chess [9][9] = {};
-	//
-	Preparation(Table_chess,p); // Размещаем фигуры на доске
-	//
-	FILE *file = 0;
-	char Name_file[30];
+    file = Checking_Resources(file, Name_file);
+    //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ  пїЅпїЅ пїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
 
-	file = Checking_Resources(file,Name_file);
-	//Проверяем на месте  ли файл с ходами
+    Checking_moves(file, p, Table_chess);
 
-	Checking_moves(file,p,Table_chess);
-
-	Print_table(Table_chess);
-	return 1;
+    Print_table(Table_chess);
+    return 1;
 }
