04827b779440ea825e34fc35c7b6f6ae99d09573
Danil <danil_ignatyev@mail.ru>
2021-04-22 11:09:43 +0700

    Modified code

--- src/chessviz/main.c
+++ src/chessviz/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <libchess/tools.h>
+#include <libchessviz/tools.h>
+#include <libchessviz/desk.h>
+#include <libchessviz/move.h>
 
 int main ()
 {
