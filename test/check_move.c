5648ef5e211ebcee2fcaa7a08b5209dd7601c8c4
Danil <danil_ignatyev@mail.ru>
2021-04-27 14:50:06 +0700

    Change code

--- test/check_move.c
+++ test/check_move.c
@@ -5,6 +5,7 @@
 #include <libchessviz/tools.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 CTEST(suite2, PUSTOI)
 {
@@ -31,16 +32,16 @@ CTEST(suite2, CHEK_MOVE)
         }
     }
 
-    Table_chess[0][1] = 'a', Table_chess[0][2] = 'b', Table_chess[0][3] = 'c',
-    Table_chess[0][4] = 'd', Table_chess[0][5] = 'e', Table_chess[0][6] = 'f',
-    Table_chess[0][7] = 'g', Table_chess[0][8] = 'h', Table_chess[1][0] = '1',
-    Table_chess[2][0] = '2', Table_chess[3][0] = '3', Table_chess[4][0] = '4',
-    Table_chess[5][0] = '5', Table_chess[6][0] = '6', Table_chess[7][0] = '7',
+    Table_chess[0][1] = 'a'; Table_chess[0][2] = 'b'; Table_chess[0][3] = 'c';
+    Table_chess[0][4] = 'd'; Table_chess[0][5] = 'e'; Table_chess[0][6] = 'f';
+    Table_chess[0][7] = 'g'; Table_chess[0][8] = 'h'; Table_chess[1][0] = '1';
+    Table_chess[2][0] = '2'; Table_chess[3][0] = '3'; Table_chess[4][0] = '4';
+    Table_chess[5][0] = '5'; Table_chess[6][0] = '6'; Table_chess[7][0] = '7';
     Table_chess[8][0] = '8';
     //Áåëàÿ êîììàíäà
-    Table_chess[1][1] = 'R', Table_chess[1][2] = 'N', Table_chess[1][3] = 'B',
-    Table_chess[1][4] = 'Q', Table_chess[1][5] = 'K', Table_chess[1][6] = 'B',
-    Table_chess[1][7] = 'N', Table_chess[1][8] = 'R';
+    Table_chess[1][1] = 'R'; Table_chess[1][2] = 'N'; Table_chess[1][3] = 'B';
+    Table_chess[1][4] = 'Q'; Table_chess[1][5] = 'K'; Table_chess[1][6] = 'B';
+    Table_chess[1][7] = 'N'; Table_chess[1][8] = 'R';
     for (p = 0; p < 8; p++) {
         Table_chess[2][p + 1] = 'P';
     }
@@ -56,4 +57,5 @@ CTEST(suite2, CHEK_MOVE)
         for (j = 0; j < 9; j++)
             ASSERT_EQUAL(Table_chess[i][j], Tablefact_chess[i][j]);
     }
+    fclose(file);
 }
